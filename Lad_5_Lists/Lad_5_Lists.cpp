226b0909be58b2d48e5f670c47b22d590f1cbe6b
Letaga <Inventor2004nn@gmail.com>
2023-12-15 20:36:18 +0300

    Добавьте файлы проекта.

---
+++ Lad_5_Lists/Lad_5_Lists.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class list
+{
+
+    class Cnot
+    {
+        Cnot* beforCnot;
+        Cnot* nextCnot;
+        T data;
+        Cnot(T t)
+        {
+            beforCnot = nextCnot = nullptr;
+            data = t;
+        }
+        Cnot()
+        {
+            beforCnot = nextCnot = nullptr;
+            data = 0;
+        }
+    };
+    Cnot* begin;
+    Cnot* end;
+    int size;
+
+public:
+    list()
+    {
+        size = 0;
+        begin = end = nollptr;
+    }
+
+};
+
+int main()
+{
+    list<char> Text;
+    list<int> Nomber;
+    cout << "hello world";
+}
