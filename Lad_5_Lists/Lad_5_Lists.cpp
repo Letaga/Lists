e91e6c426ddbb3676288d550dd1b80756356143b
Letaga <Inventor2004nn@gmail.com>
2023-12-25 03:15:46 +0300

    Add function for output list on the console

--- Lad_5_Lists/Lad_5_Lists.cpp
+++ Lad_5_Lists/Lad_5_Lists.cpp
@@ -15,17 +15,20 @@ int EntrEdge(int x, int l, int r) //Checking the validity of values
     return x;
 }
 
-
+// Класс список
 class list
 {
 private:
     class Node
     {
     public:
-        Node(int data1, Node* addres)
+        Node(int data1, Node* Next)
         {
-            data = data1;
-            nextN = addres;
+            this->data = data1;
+            this->nextN = Next;
+
+            //data = data1;
+            //nextN = Next;
         }
 
         int data;
@@ -41,7 +44,9 @@ public:
         return size;
     }
 
-    void addBeg(int value);
+    void addOne(int value); //Add node to the begging of the list
+    void addBeg(int nAdd); //Add n node to the begging
+    void cOut(); //Output list on the console
 
     list()
     {
@@ -49,55 +54,77 @@ public:
         head = nullptr;
     }
     ~list();
-
 };
 
 list::~list()
 {
     while (size)
     {
-        if (size <= 0) return;
-        Node* temp = head;
+        if (size <= 0) 
+            return;
+        Node* tmp = head;
         head = head->nextN;
-        delete temp;
+        delete tmp;
         size--;
     }
 }
 
-
-
-void list::addBeg(int value)
+void list::addOne(int value)
 {
-    if (head == nullptr)
-    {
-        head = new Node(value, nullptr);
-        size++;
-    }
-    else
-    {
-        Node tmp(value, nullptr);
-        size++;
-    }
-
+    head = new Node(value, head);
+    size++;
 }
+
+void list::addBeg(int nAdd)
+{
+    int value = 0;
+    cout << "Введите значения элементов\n";
+    for (int i = 0; i < nAdd; i++)
+    {
+        value = EntrEdge(value, INT_MIN, INT_MAX);
+        this->addOne(value);
+    }
+}
+
+void list::cOut()
+{
+    Node* tmp = head;
+    while (tmp != nullptr)
+    {
+        cout << tmp->data << " ";
+        tmp = tmp->nextN;
+    }
+    cout << endl;
+}
+
 
 // ===============================  MAIN  ===============================
 int main()
 {
     setlocale(LC_ALL, "");
 
-    int size_1 = 0, size_2 = 5;
+    int size1 = 0, size2 = 5;
 
     cout << "Введите длину списка и его значения\n size = ";
-    size_1 = EntrEdge(size_1, 0, INT_MAX);
-    list lst_1;
-    list lst_2;
+    size1 = EntrEdge(size1, 0, INT_MAX);
+    list lst1;
+    list lst2;
+    for (int i = size2 - 1; i >= 0; i--)
+    {
+        lst2.addOne(i + 5);
+    }
 
-    lst_1.addBeg(5);
-    lst_1.addBeg(7);
-    cout << "Размер списка " << lst_1.getSize();
+    lst1.addBeg(size1);
+
+    cout << "list 1 = " << endl;
+    lst1.cOut();
+
+    cout << "list 2 = " << endl;
+    lst2.cOut();
+
+    cout << "Размер списка 2 = " << lst2.getSize() << endl;
 
     cout << "End program, enter anything...";
-    cin >> size_1;
+    cin >> size1;
 }
 // ======================================================================
