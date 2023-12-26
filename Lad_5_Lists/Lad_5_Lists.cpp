577860dbbed01883be0577033f45145a4e1fd2c1
Letaga <Inventor2004nn@gmail.com>
2023-12-26 20:10:06 +0300

    Add function swap Min and Max, combining lists (sum)

--- Lad_5_Lists/Lad_5_Lists.cpp
+++ Lad_5_Lists/Lad_5_Lists.cpp
@@ -24,11 +24,8 @@ private:
     public:
         Node(int data1, Node* Next)
         {
-            this->data = data1;
-            this->nextN = Next;
-
-            //data = data1;
-            //nextN = Next;
+            data = data1;
+            nextN = Next;
         }
 
         int data;
@@ -47,6 +44,14 @@ public:
     void addOne(int value); //Add node to the begging of the list
     void addBeg(int nAdd); //Add n node to the begging
     void cOut(); //Output list on the console
+    void sort(); //Sorting a simple insert
+    void swap(Node* N1, Node* N2);
+    void swMinMax();
+    void sum(list L);
+
+    //int& operator[] (int);
+    //void addEnd(int n);//Добавление элемента в конец
+    //int get(int ind);
 
     list()
     {
@@ -60,7 +65,7 @@ list::~list()
 {
     while (size)
     {
-        if (size <= 0) 
+        if (size <= 0)
             return;
         Node* tmp = head;
         head = head->nextN;
@@ -97,6 +102,63 @@ void list::cOut()
     cout << endl;
 }
 
+void list::swap(Node* N1, Node* N2)
+{
+    int tmp = N1->data;
+    N1->data = N2->data;
+    N2->data = tmp;
+}
+
+void list::swMinMax()
+{
+    Node* max = head;
+    Node* min = head;
+    Node* cerrent = head;
+    while (cerrent != nullptr)
+    {
+        if (cerrent->data > max->data)
+            max = cerrent;
+        if (cerrent->data < min->data)
+            min = cerrent;
+        cerrent = cerrent->nextN;
+    }
+    swap(min, max);
+}
+
+void list::sort()
+{
+    int buf;
+    Node* tmp = head;
+    Node* iN = head;
+    Node* jN;
+    while (iN!=nullptr)
+    {
+        tmp = iN;
+        jN = head;
+        while (tmp->data > jN->data && jN != iN)
+        {
+            jN = jN->nextN;
+        }
+        buf = jN->data;
+        jN->data = tmp->data;
+        iN->data = buf;
+        iN = iN->nextN;
+    }
+}
+
+void list::sum(list L)
+{
+    Node* current = L.head;
+    while (current != nullptr)
+    {
+        this->addOne(current->data);
+        current = current->nextN;
+    }
+
+    this->sort();
+}
+
+
 
 // ===============================  MAIN  ===============================
 int main()
@@ -106,7 +168,7 @@ int main()
     int size1 = 0, size2 = 5;
 
     cout << "Введите длину списка и его значения\n size = ";
-    size1 = EntrEdge(size1, 0, INT_MAX);
+    size1 = EntrEdge(size1, 1, INT_MAX);
     list lst1;
     list lst2;
     for (int i = size2 - 1; i >= 0; i--)
@@ -116,15 +178,34 @@ int main()
 
     lst1.addBeg(size1);
 
+
     cout << "list 1 = " << endl;
+    lst1.cOut();
+
+    cout << "swap" << endl;
+    lst1.swMinMax();
+
+    cout << "Заданный список после перестановки минимального и максимального элементов" << endl;
+    lst1.cOut();
+
+    lst1.sort();
+    cout << "Отсортированный первый список" << endl;
     lst1.cOut();
 
     cout << "list 2 = " << endl;
     lst2.cOut();
 
-    cout << "Размер списка 2 = " << lst2.getSize() << endl;
+    lst1.sum(lst2);
+    cout << "\n\nОбьединённые списки" << endl;
+    lst1.cOut();
 
-    cout << "End program, enter anything...";
-    cin >> size1;
+    cout << "Размер объединённого списка " << lst1.getSize() << endl;
+
+     cout << "End program, enter anything...";
+     cin >> size1;
+     
+    lst1.~list();
+    lst2.~list();
+    return 0;
 }
 // ======================================================================
